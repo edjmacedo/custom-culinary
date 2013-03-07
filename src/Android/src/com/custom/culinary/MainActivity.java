9c75b630da9ed0b7b0e2bf6d01f7fbeca1753fd9
Edvan Macedo <edvan.macedo.jr@gmail.com>
2013-03-07 11:25:56 -0400

    Added the main layout
    Signed-off-by: Edvan Macedo <edvan.macedo.jr@gmail.com>

--- src/Android/src/com/custom/culinary/MainActivity.java
+++ src/Android/src/com/custom/culinary/MainActivity.java
@@ -1,22 +1,38 @@
 package com.custom.culinary;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main);			
+		
+		ListView listView = (ListView) findViewById(R.id.Recent);
+		String[] values = new String[]{"Recent01","Recent02","Recent03","Recent04"};
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+		
+		listView.setAdapter(adapter);
 	}
 
-	@Override
+	//@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void addCustomFood(View v){
+		
 	}
 
 }
